c7baba9e0dfa26d369030660a5c738a0145425d5
Scott Gaines <scottdgaines@gmail.com>
2024-01-27 12:21:21 -0700

    Update react-router from v5 to 6

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Nav.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div className='nav-bar'>
-      <p className='nav-title'>Where's Walter</p>
-      <img src={logo} className='logo' />
+      <Link to='/'>
+        <p className='nav-title'>Where's Walter</p>
+        <img src={logo} className='logo' />
+      </Link>
     </div>
   )
 }
