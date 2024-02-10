176dce36a9b72f42ac1bb32e08d1e716aab2a558
Scott Gaines <scottdgaines@gmail.com>
2024-02-10 12:20:57 -0700

    Undo dynamic button add

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,10 +6,8 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div className='nav-bar'>
-
         <p className='nav-title'>Where's Walter</p>
         <img src={logo} className='logo' />
-
     </div>
   )
 }
