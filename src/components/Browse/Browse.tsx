06bc0928657df632ec80dfaa141173aede061216
Scott Gaines <scottdgaines@gmail.com>
2024-04-18 19:11:11 -0600

    Add functionality to allow for styling-highlight when user selects category from home

--- src/components/Browse/Browse.tsx
+++ src/components/Browse/Browse.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Notice } from '../../interfaces'
 import Card from '../Card/Card'
 import './Browse.css'
@@ -7,21 +7,30 @@ import { Link } from 'react-router-dom'
 type BrowseProps = {
   lostNotices?: Notice[];
   foundNotices?: Notice[];
+  toggleSelected?: (arg: string) => void;
+  selected?: boolean;
 }
 
-const Browse: React.FC<BrowseProps> = ({ lostNotices, foundNotices }) => {
+const Browse: React.FC<BrowseProps> = ({ lostNotices, foundNotices, toggleSelected, selected }) => {
   const notices = lostNotices ? lostNotices : foundNotices
-  const title = lostNotices ? "Lost" : "Found"
-  const [selected, setSelected] = useState(false)
+  const title = lostNotices ? 'Lost' : 'Found'
   
   const cards = notices && notices.map(notice => {
     return <Card id={notice.id} notice={notice} />
   })
 
+  const handleClick = () => {
+    if (toggleSelected) {
+      toggleSelected(title)
+    }
+  }
+
+  const highlight = selected ? 'cards-container selected' : 'cards-container'
+
   return (
     <div>
-      <p className='title'>{title}</p>
-      <div className='cards-container'>
+      <p className='title' onClick={handleClick}>{title}</p>
+      <div className={highlight}>
         {cards}
       </div>
     </div>
