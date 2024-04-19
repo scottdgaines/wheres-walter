ce841dab2ed2b5a5e7b114ae34027aff434f101d
Scott Gaines <scottdgaines@gmail.com>
2024-04-18 21:13:07 -0600

    Add plus icon image

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+
+
+  return (
+    <div>
+      <form>
+        <input type='drop' />
+        <label>Pet Name</label>
+      </form>
+    </div>
+  )
+}
+
+export default Form
+
+// id: 12,
+//         noticeType: 'Found',
+//         petName: 'Tum Tums',
+//         images:['https://www.catster.com/wp-content/uploads/2023/11/Calico-Cat-2.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQETVpX-ScEmRQA6Y77k3myOBgmUJQx4FEzkDbQUPJwUSMFDYVjFWMWFwW7-HhPnaqzhzM&usqp=CAU'],
+//         petBreed: 'Calico',
+//         petSpecie: 'Cat',
+//         petDescription: 'Orange, black, and white calico, with green eyes',
+//         dateLost: '03/17/2023',
+//         chipNum: 1234567,
+//         petNotes: 'Found near the corner of 10th and irving',
+//         contactNum: '123-456-7890',
+//         contactEmail: 'example@example.com',
+//         reward: null
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Nav.css'
 import logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom'
+import formLogo from '../../assets/form-logo.png'
 
 const Nav = () => {
   return (
     <div className='nav-bar'>
         <p className='nav-title'>Where's Walter</p>
         <img src={logo} className='logo' />
+        <Link to='/form' >
+          <img src={formLogo} className='form-logo' />
+        </Link>
     </div>
   )
 }
