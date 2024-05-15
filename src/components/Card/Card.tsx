6978cb331d0f94625b36f60fa72d9b14bc28ef66
Scott Gaines <scottdgaines@gmail.com>
2024-05-14 19:58:29 -0600

    Add tag to cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,13 +9,16 @@ type CardProps = {
 }
 
 const Card: React.FC<CardProps> = ({ id, notice }) => {
-  const navigate = useNavigate()
+  const tagColor = notice.noticeType === 'Lost' ? 'lost' : 'found'
 
   return (
     <Link to={`/${id}`} className='link'>
-      <div className='card'>
-        <h3 className='card-name'>{notice.petName}</h3>
-        <img src={notice.images[0]} className='thumbnail' />
+      <div className='card-hover-container'>
+        <div className={`card-tag ${tagColor}`}>{notice.noticeType}</div>
+        <div className='card'>
+          <h3 className='card-name'>{notice.petName}</h3>
+          <img src={notice.images[0]} className='thumbnail' />
+        </div>
       </div>
     </Link>
   )
