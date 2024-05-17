34fca5b3130a08549a35230b4d145f4c1c2470a5
Scott Gaines <scottdgaines@gmail.com>
2024-05-17 17:46:02 -0600

    Cleanup Card.tsx and Card.css

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { Notice } from '../../interfaces'
-import './Card.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { NoticeInterface } from '../../interfaces';
+import './Card.css';
 
 type CardProps = {
     id: number,
-    notice: Notice;
-}
+    notice: NoticeInterface
+};
 
 const Card: React.FC<CardProps> = ({ id, notice }) => {
-  const tagColor = notice.noticeType === 'Lost' ? 'lost' : 'found'
+  const tagColor = notice.noticeType === 'Lost' ? 'lost' : 'found';
 
   return (
     <Link to={`/${id}`} className='link'>
       <div className='card-hover-container'>
         <div className={`card-tag ${tagColor}`}>{notice.noticeType}</div>
         <div className='card'>
-          <h3 className='card-name'>{notice.petName}</h3>
+          <h3>{notice.petName}</h3>
           <img src={notice.images[0]} className='thumbnail' />
         </div>
       </div>
     </Link>
   )
-}
+};
 
 export default Card
